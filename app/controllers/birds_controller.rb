9eb95a979d43b499bf76297121188d3d124b67c0
lilian <lilian.wangechi@student.moringaschool.com>
2022-11-26 14:39:07 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,13 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    # get the data from the body of the request
+    # create a new Bird
+    bird = Bird.create(name: params[:name], species: params[:species])
+    # send the newly create Bird as a response
+    render json: bird, status: :created
+  end
+
+
 end
